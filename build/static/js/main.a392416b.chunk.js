de420d3bca442292106053db8ea26caf95089d2e
Damian Wróblewski <01153105@pw.edu.pl>
2021-01-25 18:00:46 +0100

    init commit

---
+++ build/static/js/main.a392416b.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-app"]=this["webpackJsonpreact-app"]||[]).push([[0],{13:function(e,t,n){"use strict";n.r(t);var c=n(1),r=n(0),a=n.n(r),p=n(3),i=n.n(p),s=n(4),o=n(5),u=n(7),j=n(6),b=function(e){Object(u.a)(n,e);var t=Object(j.a)(n);function n(){return Object(s.a)(this,n),t.apply(this,arguments)}return Object(o.a)(n,[{key:"render",value:function(){return Object(c.jsx)("div",{children:Object(c.jsx)("h1",{children:"App"})})}}]),n}(a.a.Component);i.a.render(Object(c.jsx)(b,{}),document.getElementById("root"))}},[[13,1,2]]]);
+//# sourceMappingURL=main.a392416b.chunk.js.map
