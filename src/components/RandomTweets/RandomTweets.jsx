27a30121cf8cce01af257d97c71018629b9da43c
Damian Wróblewski <01153105@pw.edu.pl>
2021-01-26 18:58:37 +0100

    Imports

--- src/components/RandomTweets/RandomTweets.jsx
+++ src/components/RandomTweets/RandomTweets.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Card, CardContent, Typography, Grid}  from '@material-ui/core';
+import cx from 'classnames';
+
+import styles from './RandomTweets.module.css';
 
 const RandomTweets = () => {
     return (
-        <h1>RandomTweets</h1>
+        <div>
+            <h1>Test</h1>
+        </div>
     )
 }
 
