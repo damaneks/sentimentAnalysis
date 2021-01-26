4715deae1da5d31f71bc9a371407d0c4cba703d2
Damian Wróblewski <01153105@pw.edu.pl>
2021-01-26 19:05:50 +0100

    Revert "Add Grid container"
    This reverts commit 1ed77d7ea32bccb62f133c9110b7df64463d4189.

--- src/components/RandomTweets/RandomTweets.jsx
+++ src/components/RandomTweets/RandomTweets.jsx
@@ -6,15 +6,8 @@ import styles from './RandomTweets.module.css';
 
 const RandomTweets = () => {
     return (
-        <div className={styles.container}>
-            <Grid container spacing={3} justify="center">
-                <Grid item component={Card} xs={12} md={12} className={cx(styles.card, styles.positive)}>
-                    <CardContent>
-                        <Typography color="textSecondary" gutterBottom>Losowy pozytywny tweet</Typography>
-                        <Typography variant="body2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor in</Typography>
-                    </CardContent>
-                </Grid>
-            </Grid>
+        <div>
+            <h1>Test</h1>
         </div>
     )
 }
