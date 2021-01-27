b33985c9372797bfa977723361bbc4adf0ab951f
Damian Wróblewski <01153105@pw.edu.pl>
2021-01-27 09:45:34 +0100

    Changeable charts

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import React from 'react';
 import { Cards, Chart, HashtagPicker, RandomTweets } from './components';
 import styles from './App.module.css';
 import { fetchData } from './api';
+import { BottomNavigation, BottomNavigationAction } from '@material-ui/core';
+import BarChartIcon from '@material-ui/icons/BarChart';
+import ShowChartIcon from '@material-ui/icons/ShowChart';
 
 class App extends React.Component {
 
     state = {
         data: {},
         hashtag: 'global',
+        chartType: 1,
     }
 
     async componentDidMount() {
@@ -23,15 +27,24 @@ class App extends React.Component {
         this.setState({data: fetchedData, hashtag: hashtag});
     }
 
+    handleChartChange = (event, newValue) => {
+        this.setState({chartType: newValue})
+    }
+    
     render() {
         const {data} = this.state;
         const {hashtag} = this.state;
+        const {chartType} = this.state;
         
         return (
             <div className={styles.container}>
                 <Cards data={data}/>
                 <HashtagPicker handleHashtagChange={this.handleHashtagChange}/>
-                <Chart hashtag={hashtag}/>
+                <BottomNavigation value={this.state.chartType} onChange={this.handleChartChange} className={styles.nav}>
+                    <BottomNavigationAction label={"Line"} value={0} icon={<ShowChartIcon />}/>
+                    <BottomNavigationAction label={"Bar"} value={1} icon={<BarChartIcon />}/>
+                </BottomNavigation>
+                <Chart hashtag={hashtag} chartType={chartType}/>
                 <RandomTweets data={data}/>
             </div>
         )
